de2a98cbaa9b05042de4c9cc2a94f00e90ba9c7e
Diego Almeida <diego.almeida@outlook.com>
2021-04-23 11:29:54 -0300

    Isolamento de teste criado

--- tests/Controller/GetUserActionTest.php
+++ tests/Controller/GetUserActionTest.php
@@ -2,31 +2,53 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\ORM\Tools\ToolsException;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 class GetUserActionTest extends WebTestCase
 {
-    public function test_get_user_should_return_200(): void
+
+    private EntityManagerInterface $em;
+    private KernelBrowser $client;
+
+    protected function setUp(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/users/1');
+        $this->client = self::createClient();
+        $this->em = self::$kernel->getContainer()->get('doctrine')->getManager();
+        $tool = new SchemaTool($this->em);
+        $metadata = $this->em->getClassMetadata(User::class);
+        $tool->dropDatabase();
+        try {
+            $tool->createSchema([$metadata]);
+        } catch (ToolsException $e) {
+            $this->fail('Impossível criar o banco de dados: "'.$e->getMessage().'"');
+        }
+    }
 
-        $statusCode = $client->getResponse()->getStatusCode();
+    public function test_get_user_should_return_200(): void
+    {   
+        $user = new User();
+        $user->setNome('Diego');
+        $user->setSobrenome('Almeida');
+        $user->setEmail('diego.almeida@outlook.com');
+        $this->em->persist($user);
+        $this->em->flush();
 
+        $this->client->request('GET', '/users/1');
+        $statusCode = $this->client->getResponse()->getStatusCode();
         $this->assertSame(Response::HTTP_OK, $statusCode);
-
     }
 
     public function test_get_user_should_return_404(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/users/999');
-
-        $statusCode = $client->getResponse()->getStatusCode();
-
+        $this->client->request('GET', '/users/999');
+        $statusCode = $this->client->getResponse()->getStatusCode();
         $this->assertSame(Response::HTTP_NOT_FOUND, $statusCode);
-
     }
 
 }
