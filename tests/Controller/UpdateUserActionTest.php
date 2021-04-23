a66a2705604a289e0da5fec9d20b5cf94762ef80
Diego Almeida <diego.almeida@outlook.com>
2021-04-23 11:34:03 -0300

    Isolamento de teste ao atualizar um usuário

--- tests/Controller/UpdateUserActionTest.php
+++ tests/Controller/UpdateUserActionTest.php
@@ -2,15 +2,45 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\ORM\Tools\ToolsException;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 class UpdateUserActionTest extends WebTestCase
 {
+
+    private EntityManagerInterface $em;
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = self::createClient();
+        $this->em = self::$kernel->getContainer()->get('doctrine')->getManager();
+        $tool = new SchemaTool($this->em);
+        $metadata = $this->em->getClassMetadata(User::class);
+        $tool->dropDatabase();
+        try {
+            $tool->createSchema([$metadata]);
+        } catch (ToolsException $e) {
+            $this->fail('Impossível criar o banco de dados: "'.$e->getMessage().'"');
+        }
+    }
+
     public function test_update_user(): void
     {
-        $client = static::createClient();
-        $client->request(
+        
+        $user = new User();
+        $user->setNome('Diego');
+        $user->setSobrenome('Almeida');
+        $user->setEmail('diego.almeida@outlook.com');
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $this->client->request(
             method: 'PUT',
             uri: '/users/1',
             content: json_encode([
@@ -20,7 +50,7 @@ class UpdateUserActionTest extends WebTestCase
             ])
         );
 
-        $statusCode = $client->getResponse()->getStatusCode();
+        $statusCode = $this->client->getResponse()->getStatusCode();
 
         $this->assertSame(Response::HTTP_NO_CONTENT, $statusCode);
     }
