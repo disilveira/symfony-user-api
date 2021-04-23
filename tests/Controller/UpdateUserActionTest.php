88092b88a2832daaa9478cd6ef46219534b4850d
Diego Almeida <diego.almeida@outlook.com>
2021-04-23 11:36:59 -0300

    Correção no isolamento do teste de atualização de usuário

--- tests/Controller/UpdateUserActionTest.php
+++ tests/Controller/UpdateUserActionTest.php
@@ -54,22 +54,4 @@ class UpdateUserActionTest extends WebTestCase
 
         $this->assertSame(Response::HTTP_NO_CONTENT, $statusCode);
     }
-
-    public function test_update_user_with_user_not_found(): void
-    {
-        $client = static::createClient();
-        $client->request(
-            method: 'PUT',
-            uri: '/users/99',
-            content: json_encode([
-                'nome' => 'Diego',
-                'sobrenome' => "Almeida",
-                'email' => 'diego.almeida@outlook.com'
-            ])
-        );
-
-        $statusCode = $client->getResponse()->getStatusCode();
-
-        $this->assertSame(Response::HTTP_NOT_FOUND, $statusCode);
-    }
 }
